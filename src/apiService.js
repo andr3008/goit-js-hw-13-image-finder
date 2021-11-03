6b8522b027d4f213a48283a6099365d769585fb7
Andrei Potapov <andr3008@gmail.com>
2021-11-03 22:55:25 +0200

    add fetch to API service,template,pagination

---
+++ src/apiService.js
@@ -0,0 +1,31 @@
+const BASE_URL = 'https://pixabay.com/api';
+const API_URL = '24155040-09394a9154ec72d89d619259b';
+
+export default class PixabayApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  fetchPhotos() {
+    return fetch(
+      `${BASE_URL}/?key=${API_URL}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}`,
+    )
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
+import '@pnotify/core/dist/BrightTheme.css';
+const { error } = require('@pnotify/core');
+
 import './sass/main.scss';
+import PixabayApiService from './apiService';
+import hitsTemp from './templates/hitsTemp.hbs';
+import LoadMoreBtn from './loadMoreBtn';
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  galleryList: document.querySelector('.gallery'),
+  // loadMore: document.querySelector('[data-action="load-more"]'),
+};
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+const pixabayApiService = new PixabayApiService();
+
+console.log(loadMoreBtn);
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+  clearHitsContainer();
+  pixabayApiService.query = e.currentTarget.elements.query.value;
+  if (pixabayApiService.query === '' || pixabayApiService.query === 'Space') {
+    return error({
+      text: 'Enter text!',
+      delay: 300,
+    });
+  }
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
+  pixabayApiService.resetPage();
+  pixabayApiService.fetchPhotos().then(appendHitsMarkup);
+  loadMoreBtn.enable();
+}
+function onLoadMore() {
+  loadMoreBtn.disable();
+  pixabayApiService.fetchPhotos().then(appendHitsMarkup);
+  loadMoreBtn.enable();
+}
+function fetchHits() {}
+function appendHitsMarkup(hits) {
+  refs.galleryList.insertAdjacentHTML('beforeend', hitsTemp(hits));
+}
+function clearHitsContainer() {
+  refs.galleryList.innerHTML = '';
+}
